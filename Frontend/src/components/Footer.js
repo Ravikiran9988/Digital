cbf1e46b9abb958aca1be086e6a1db006f66b97d
Ravikiran9988 <vk8896992@gmail.com>
2024-12-21 20:05:25 +0530

    Create Footer.js

---
+++ Frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+// src/components/Footer.js
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="App-footer">
+        <p>© 2024 Digital Wallet. All rights reserved.</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
