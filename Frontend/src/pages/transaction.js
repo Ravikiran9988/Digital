01c10610192ca3e3c2b052b5705370d47e42ec46
Ravikiran9988 <vk8896992@gmail.com>
2024-12-21 19:43:18 +0530

    Create transaction.js

---
+++ Frontend/src/pages/transaction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Transaction() {
+  return (
+    <div>
+      <h1>Transaction Page</h1>
+      <p>View and manage your transactions here.</p>
+    </div>
+  );
+}
+
+export default Transaction;
