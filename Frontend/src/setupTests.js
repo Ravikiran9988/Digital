1835865d2496af1f134208cb7ab91dd4a60aa4a0
Ravikiran9988 <vk8896992@gmail.com>
2024-12-21 19:50:32 +0530

    Create setupTests.js

---
+++ Frontend/src/setupTests.js
@@ -0,0 +1,7 @@
+// src/setupTests.js
+
+import '@testing-library/jest-dom/extend-expect'; // for better assertions in tests
+
+// You can also add other global setups here (e.g., mock functions, fetch, etc.)
+// Example: 
+// global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve({}) }));
